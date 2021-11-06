341c01c45b6079491dde9d3b7af79e4f0748c6a8
JOSÉ LUIS MUÑOZ PADILLA <USIS043721@ugb.edu.sv>
2021-11-06 17:21:32 -0600

    Agregar archivos de proyecto.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Grupo4_PaginaWeb.Global" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Grupo4_PaginaWeb.Startup))]
+namespace Grupo4_PaginaWeb
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
